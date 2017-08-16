f3d012a6e7900455b571eb2f3147350f31dd9821
vikas1369 <do.vikas1369@gmail.com>
2017-08-16 16:49:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article1',function(req,res){
+    res.sendFile("Article one is requested. I am article 1");
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
