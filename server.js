d94886300e982ff3e27e7a1cb6a0f878f5571d77
vikas1369 <do.vikas1369@gmail.com>
2017-08-16 18:36:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,9 +100,6 @@ app.get('/:articleName',function(req,res){
     var articleName=req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
-app.get('/article-two',function(req,res){
-    res.sendFile(path.join(__dirname,'ui','article-two.html'));
-});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
