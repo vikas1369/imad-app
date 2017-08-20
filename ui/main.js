a8e47e12401c97512acd5fab930e4899ebe791ba
vikas1369 <do.vikas1369@gmail.com>
2017-08-20 21:33:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var img=document.getElementById('img');
 var marginLeft=0;
 img.onclick=function(){
-  var interval=setInterval(function(){
+  setInterval(function(){
       marginLeft+=10;
       img.style.marginLeft=marginLeft+'px';  
   }
