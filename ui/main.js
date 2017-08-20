1cb9089fc1a1551e82b528e79596283624e04f56
vikas1369 <do.vikas1369@gmail.com>
2017-08-20 23:30:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,17 @@
 var button=document.getElementById('counter');
-var counter=0;
+
 button.onclick=function(){
-    counter+=1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
+    var request=XMLHttpRequest();
+    request.onreadystatechange=function(){
+      if(request.readyState==XMLHttpRequest.DONE){
+          if(request.status==200){
+              var counter=request.responseText
+              var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+          }
+          //take some action
+      }  
+      //Not done
+    };
+    request.open('GET','http://dovikas1369.imad.hasura-app.io/counter');
 };
