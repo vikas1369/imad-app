fd381ef3ac602d112a617b5d12bd0af8ed370006
vikas1369 <do.vikas1369@gmail.com>
2017-08-20 21:24:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var img=document.getElementById('img');
 img.onclick=function(){
   setInterval(function(){
-      img.style.marginLeft='10px';  
+      img.style.marginLeft=img.style.marginLeft+'10px';  
   }
   ,100);
 };
