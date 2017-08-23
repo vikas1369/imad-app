7617ea0b17eca66a8e0ee0839787b55a0107e249
vikas1369 <do.vikas1369@gmail.com>
2017-08-23 21:00:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -120,7 +120,7 @@ app.get('/test-db',function(req,res){
 });
 app.get('/articles/:articleName',function(req,res){
     var articleName=req.params.articleName;
-    poll.query("SELECT * FROM article WHERE title="+req.params.articleName,function(err,result){
+    pool.query("SELECT * FROM article WHERE title="+req.params.articleName,function(err,result){
        if(err){
            res.status(500).send(err.toString());
        } else{
